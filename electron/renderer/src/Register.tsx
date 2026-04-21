413a672174e868526ea6add2080a058625a238ca
innopandapteltd <dev@innopanda.com>
2026-04-21 14:17:22 +0530

    error issue

---
+++ electron/renderer/src/Register.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import { authService } from './services/authService'
+import RegisterForm from './RegisterForm';
+
+
+export default function Register({ onToggle }: { onToggle: () => void }) {
+  const [loading, setLoading] = useState(false)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault()
+    if (loading) return
+    
+    setLoading(true)
+    try {
+      await authService.registerUser(formData)
+      alert('Account created successfully! Please login.')
+      onToggle()
+    } catch (err: any) {
+      console.error("Register Error:", err.message)
+      alert(err.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#0f172a] text-white p-4">
+      <RegisterForm 
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={handleRegister}
+        loading={loading}
+        onToggle={onToggle}
+      />
+    </div>
+  )
+}
