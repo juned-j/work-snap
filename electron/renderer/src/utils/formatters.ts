7c9df76b13c1116391b12c1c2d62476febb18d66
innopandapteltd <dev@innopanda.com>
2026-04-21 14:00:03 +0530

    error issue

---
+++ electron/renderer/src/utils/formatters.ts
@@ -0,0 +1,6 @@
+export const formatTime = (s: number) => {
+  const h = Math.floor(s / 3600).toString().padStart(2, '0');
+  const m = Math.floor((s % 3600) / 60).toString().padStart(2, '0');
+  const sec = (s % 60).toString().padStart(2, '0');
+  return `${h}:${m}:${sec}`;
+};
