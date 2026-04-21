7ee0c2c7580c0e0e4794f560fe8b1c5b2311c7d1
innopandapteltd <dev@innopanda.com>
2026-04-21 12:13:04 +0530

    Full width  register page css fix

--- electron/preload.js
+++ electron/preload.js
@@ -3,5 +3,8 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('electron', {
   startSession: () => ipcRenderer.invoke('start-session'),
   stopSession: () => ipcRenderer.invoke('stop-session'),
-  captureScreenshot: () => ipcRenderer.invoke('capture-screenshot') // ✅ no param
+
+  captureScreenshot: () => ipcRenderer.invoke('capture-screenshot'),
+
+  getSystemIdleTime: () => ipcRenderer.invoke('get-system-idle-time')
 })
