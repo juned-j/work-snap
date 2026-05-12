baf23f6b20dddcd244d9ea0f7da18a19072ae972
juned-j <junedansari9617@gmail.com>
2026-05-12 14:34:32 +0530

    add file

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,7 @@
 use App\Http\Controllers\WorkSessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/dashboard');
 
-Route::get('/dashboard', [WorkSessionController::class, 'index'])->name('dashboard');
-Route::post('/worksnap/start', [WorkSessionController::class, 'start']);
-Route::post('/worksnap/pause', [WorkSessionController::class, 'pause']);
-Route::post('/worksnap/stop', [WorkSessionController::class, 'stop']);
-Route::post('/dashboard/session/start', [WorkSessionController::class, 'start'])->name('dashboard.session.start');
-Route::post('/dashboard/session/pause/{id}', [WorkSessionController::class, 'pause'])->name('dashboard.session.pause');
-Route::post('/dashboard/session/resume/{id}', [WorkSessionController::class, 'resume'])->name('dashboard.session.resume');
-Route::post('/dashboard/session/stop/{id}', [WorkSessionController::class, 'stop'])->name('dashboard.session.stop');
+Route::get('/', function () {
+    return redirect('/admin');
+});
