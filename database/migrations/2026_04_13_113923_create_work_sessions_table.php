6c1dc4f0d3903f3d03d05e7fe37ebc330541b447
juned-j <junedansari9617@gmail.com>
2026-05-26 13:13:24 +0530

    fix: updated worksession table  changes

--- database/migrations/2026_04_13_113923_create_work_sessions_table.php
+++ database/migrations/2026_04_13_113923_create_work_sessions_table.php
@@ -12,25 +12,47 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('work_sessions', function (Blueprint $table) {
-            $table->id(); 
+
+            $table->id();
+
+          
+            $table->foreignUuid('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
 
             
-            $table->foreignUuid('user_id')->constrained('users')->onDelete('cascade');
-            
-            $table->timestampTz('start_time')->useCurrent();
-            $table->timestampTz('end_time')->nullable();
-            
-            $table->string('status')->default('active'); 
-            
-            $table->boolean('is_active')->default(true);
+            $table->timestampTz('start_time')
+                ->useCurrent();
+
+            $table->timestampTz('end_time')
+                ->nullable();
+
+           
+            $table->string('status')
+                ->default('active');
+
+            $table->boolean('is_active')
+                ->default(true);
+
+     
+            $table->timestampTz('paused_at')
+                ->nullable();
+
+            $table->integer('total_paused_seconds')
+                ->default(0);
+
+          
+            $table->string('ip_address')
+                ->nullable();
+
+            $table->text('user_agent')
+                ->nullable();
 
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+   //update 
     public function down(): void
     {
         Schema::dropIfExists('work_sessions');
